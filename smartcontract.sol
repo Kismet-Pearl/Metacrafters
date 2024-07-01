8d0d76c8b25bef170c59f895d2d0dd4336f74427
Kismet Vallesteros <104303068+Kismet-Pearl@users.noreply.github.com>
2024-07-02 01:48:52 +0800

    Update smartcontract.sol

--- smartcontract.sol
+++ smartcontract.sol
@@ -2,31 +2,35 @@
 pragma solidity ^0.8.0;
 
 contract RequireAssertRevert {
-    address public owner;
-    uint256 public value;
+    address public owner;   // Public variable to store the owner's address
+    uint256 public value;   // Public variable to store the value
 
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender;  // Initializes the contract creator as the owner
     }
 
     modifier onlyOwner() {
+        // Modifier to restrict access to functions only to the owner
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 
     function setValue(uint256 _value) public onlyOwner {
-        require(_value > 0, "Value must be greater than zero");
-        value = _value;
+        // Function to set a new value, restricted to the owner
+        require(_value > 0, "Value must be greater than zero");  // Requires value to be greater than zero
+        value = _value;  // Sets the new value
     }
 
     function checkValue() public view {
-        assert(value != 0);
+        // Function to check if the value is not zero
+        assert(value != 0);  // Asserts that the value should not be zero
     }
 
     function resetValue() public onlyOwner {
+        // Function to reset the value to zero, restricted to the owner
         if (value == 0) {
-            revert("Value is already zero");
+            revert("Value is already zero");  // Reverts if the value is already zero
         }
-        value = 0;
+        value = 0;  // Resets the value to zero
     }
 }
